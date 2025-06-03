344ad5e84daaae5ab8314cde23bd558bc70db6b8
Knockn942 <brokervcc@gmail.com>
2025-06-03 22:24:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-TOKEN = "."  # Ersetze mit deinem Token
+TOKEN = "MTM3OTUxMzA3NjE4OTEwNjI2OA.GPLCM-.0r05FZP89-EA1Mymced1cuwxB74SO_ehSEUwdc"  # Ersetze mit deinem Token
 
 intents = discord.Intents.default()
 intents.message_content = True  # Wichtig, sonst funktionieren keine Nachrichten
